8c822ae139a29dc64b207b7a4790c51923ed2316
Martin Švrčina <svrcina.mar@gmail.com>
2018-08-16 01:41:49 +0200

    rendering + gitignore

---
+++ Terminal/Scenes/Container.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Terminal
+{
+    abstract class Container
+    {
+
+        public void Render()
+        {
+            RenderContainer();
+        }
+
+        public abstract void RenderContainer();
+
+    }
+}
---
+++ Terminal/Scenes/Scene.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Terminal
+{
+    abstract class Scene
+    {
+        Container SelectedContainer;
+        List<Container> Containers;
+
+        public Scene()
+        {
+            Containers = new List<Container>();
+            Init();
+        }
+
+        public abstract void Init();
+
+        public void Render()
+        {
+            RenderScene();
+
+            foreach(Container container in Containers)
+            {
+                container.Render();
+            }
+
+        }
+
+        public abstract void RenderScene();
+
+        public void HandleInput(ConsoleKey key)
+        {
+            HandleInput(key);
+        }
+
+        public abstract void HandleInputScene(ConsoleKey key);
+
+        private void SelectContainer(Container container)
+        {
+
+        }
+    }
+}
